bffc10cfe7877306243e28f06f59f9e9f8e43eec
pawan-kavinda <pawankavinda09@gmail.com>
2025-07-19 21:33:03 +0530

    test cases

--- serial_method.c
+++ serial_method.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+
 double f(double x) {
     return x * x;
 }
@@ -13,16 +14,32 @@ double trapezoidal_serial(double a, double b, int n) {
     return sum * h;
 }
 
-int main() {
-    double a = 0.0, b = 1.0;
-    int n = 1000000;
+int main() {  
+    double test_cases[][3] = {
+        {0.0, 1.0, 1000000},
+        {0.0, 5.0, 2000000},
+        {1.0, 3.0, 500000},
+        {0.0, 10.0, 10000000},
+        {2.0, 4.0, 100000},
+        {0.0, 1000.0, 1000000000}
+    };
+    int num_tests = sizeof(test_cases) / sizeof(test_cases[0]);
 
-    clock_t start = clock();  
+    for (int t = 0; t < num_tests; t++) {
+        double a = test_cases[t][0];
+        double b = test_cases[t][1];
+        int n = (int)test_cases[t][2];
 
-    double result = trapezoidal_serial(a, b, n);
-    clock_t end = clock();   
-    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Serial Result = %f\n", result);
-    printf("Execution Time = %f seconds\n", time_taken);
+        clock_t start = clock();
+        double result = trapezoidal_serial(a, b, n);
+        clock_t end = clock();
+
+        double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+        printf("Test %d: a=%.2f, b=%.2f, n=%d\n", t + 1, a, b, n);
+        printf("Serial Result = %.10f\n", result);
+        printf("Execution Time = %f seconds\n\n", time_taken);
+    }
+
     return 0;
 }
