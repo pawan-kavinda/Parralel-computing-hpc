266408c26809d778673b168475db3d1180d8fa47
pawan-kavinda <pawankavinda09@gmail.com>
2025-07-19 21:32:57 +0530

    multiple runs

--- openMP_method.c
+++ openMP_method.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 
 double f(double x) {
-    return x * x;  
+    return x * x;
 }
 
 double trapezoidal_openMP(double a, double b, int n) {
@@ -17,17 +17,30 @@ double trapezoidal_openMP(double a, double b, int n) {
     return sum * h;
 }
 
-int main() {
-    double a = 0.0, b = 1.0;
-    int n = 1000000;
+int main() {     
+    double test_cases[][3] = {
+        {0.0, 1.0, 1000000},
+        {0.0, 5.0, 2000000},
+        {1.0, 3.0, 500000},
+        {0.0, 10.0, 10000000},
+        {2.0, 4.0, 100000},
+        {0.0, 1000.0, 1000000000}
+    };
+    int num_tests = sizeof(test_cases) / sizeof(test_cases[0]);
 
-    double start = omp_get_wtime();  
+    for (int t = 0; t < num_tests; t++) {
+        double a = test_cases[t][0];
+        double b = test_cases[t][1];
+        int n = (int)test_cases[t][2];
 
-    double result = trapezoidal_openMP(a, b, n);
+        double start = omp_get_wtime();
+        double result = trapezoidal_openMP(a, b, n);
+        double end = omp_get_wtime();
 
-    double end = omp_get_wtime();  
-    
-    printf("OpenMP Result = %f\n", result);
-    printf("Execution Time = %f seconds\n", end - start);
+        printf("Test %d: a=%.2f, b=%.2f, n=%d\n", t + 1, a, b, n);
+        printf("OpenMP Result = %.10f\n", result);
+        printf("Execution Time = %f seconds\n\n", end - start);
+    }
+
     return 0;
 }
